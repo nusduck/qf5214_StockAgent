2e1af7985746c1bc038a4cac63d2e0d338ce3ee6
Chen Zhaoqi <E1351281@u.nus.edu>
2025-02-05 16:50:24 +0800

    Update stock_news.sql

--- database/ddl/stock_news.sql
+++ database/ddl/stock_news.sql
@@ -1,10 +1,3 @@
-CREATE TABLE demo_company_information_table (
-    company_name VARCHAR(255) NOT NULL,   -- Company name
-    industry VARCHAR(255),                -- Industry of the company
-    market_cap DECIMAL(18, 2),            -- Market capitalization
-    description TEXT,                     -- Description of the company
-    etl_date DATE                         -- ETL date, indicating when the data was extracted and loaded
-);
 
 CREATE TABLE demo_stock_news_table (
     stock_symbol VARCHAR(50) NOT NULL,    -- Stock ticker symbol (e.g., "600519" for Kweichow Moutai)
