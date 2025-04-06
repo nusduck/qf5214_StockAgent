45d787e08ef0614f127510dbce9c2118dbb29e92
HE XINYAO <E1351285@u.nus.edu>
2025-04-06 22:34:12 +0800

    Delete frontend directory

--- frontend/app/recommendation/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-      <div className="flex justify-center items-center min-h-[200px]">
-        <div className="text-center p-4 text-white">加载中...</div>
-      </div>
-    );
-  } 
--- frontend/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const STOCK_CONFIG = {
-  DEFAULT_CODE: '600519'  // 默认股票代码（茅台）
-} 
